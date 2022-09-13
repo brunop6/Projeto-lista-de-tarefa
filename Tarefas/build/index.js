0cf01da8f773ebb671c834adf4f581d78bb8e4b4
brunop6 <peresbruno6@gmail.com>
2022-09-12 23:31:05 -0300

    Kanban

--- Tarefas/build/index.js
+++ Tarefas/build/index.js
@@ -1,30 +1,58 @@
 "use strict";
-let listElement = document.querySelector("#app ul");
-let inputElement = document.querySelector("#app input");
-let buttonElement = document.querySelector("#app button");
+let inputElement = document.querySelector("input");
+let selectElement = document.querySelector("#stage");
+let buttonElement = document.querySelector("#add");
+let todoElement = document.querySelector(".toDo ul");
+let inProgressElement = document.querySelector(".inProgress ul");
+let doneElement = document.querySelector(".done ul");
 let listaSalva = localStorage.getItem("@listagemTarefas");
 let tarefas = listaSalva !== null && JSON.parse(listaSalva) || [];
 function listarTarefas() {
-    listElement.innerHTML = "";
+    todoElement.innerHTML = "";
+    inProgressElement.innerHTML = "";
+    doneElement.innerHTML = "";
     tarefas.map((item, index) => {
-        let todoElement = document.createElement("li");
-        let tarefaText = document.createTextNode(item);
-        let editElement = document.createElement("a");
-        let editText = document.createTextNode("Editar");
-        let deleteElement = document.createElement("a");
-        let deleteText = document.createTextNode("Excluir");
-        editElement.setAttribute("href", "#");
-        editElement.setAttribute("onclick", `editarTarefa(${index})`);
-        editElement.setAttribute("style", "color: Orange;");
-        deleteElement.setAttribute("href", "#");
-        deleteElement.setAttribute("onclick", `deletarTarefa(${index})`);
-        deleteElement.setAttribute("style", "color: Red;");
-        editElement.appendChild(editText);
-        deleteElement.appendChild(deleteText);
-        todoElement.appendChild(tarefaText);
-        todoElement.appendChild(editElement);
-        todoElement.appendChild(deleteElement);
-        listElement.appendChild(todoElement);
+        if (index % 2 == 0) {
+            let liElement = document.createElement("li");
+            let liText = document.createTextNode(item);
+            let aDiv = document.createElement("div");
+            let editElement = document.createElement("a");
+            let editText = document.createTextNode("Editar");
+            let deleteElement = document.createElement("a");
+            let deleteText = document.createTextNode("Excluir");
+            let moveElement = document.createElement("a");
+            let spanElement = document.createElement("span");
+            editElement.setAttribute("href", "#");
+            editElement.setAttribute("onclick", `editarTarefa(${index})`);
+            editElement.setAttribute("style", "color: Orange;");
+            editElement.appendChild(editText);
+            deleteElement.setAttribute("href", "#");
+            deleteElement.setAttribute("onclick", `deletarTarefa(${index})`);
+            deleteElement.setAttribute("style", "color: Red;");
+            deleteElement.appendChild(deleteText);
+            spanElement.innerHTML = "&#10149;";
+            moveElement.setAttribute("href", "#");
+            moveElement.setAttribute("onclick", `moverTarefa(${index})`);
+            moveElement.setAttribute("style", "color: Black;");
+            moveElement.setAttribute("class", "arrow");
+            moveElement.appendChild(spanElement);
+            aDiv.appendChild(editElement);
+            aDiv.appendChild(deleteElement);
+            aDiv.appendChild(moveElement);
+            liElement.appendChild(liText);
+            liElement.appendChild(aDiv);
+            switch (tarefas[index + 1]) {
+                case "0":
+                    todoElement.appendChild(liElement);
+                    break;
+                case "1":
+                    inProgressElement.appendChild(liElement);
+                    break;
+                case "2":
+                    doneElement.appendChild(liElement);
+                    break;
+            }
+        }
     });
 }
 function adicionarTarefa() {
@@ -34,15 +62,17 @@ function adicionarTarefa() {
     }
     else {
         let tarefaDigitada = inputElement.value;
-        tarefas.push(tarefaDigitada);
+        let estagioTarefa = selectElement.value;
+        tarefas.push(tarefaDigitada, estagioTarefa);
         inputElement.value = "";
+        selectElement.selectedIndex = 0;
         listarTarefas();
         salvarDados();
     }
 }
 function editarTarefa(posicao) {
     let newText = prompt("Renomeie a tarefa:");
-    if (newText.length < 3) {
+    if ((typeof (newText) !== "string") || (newText.length < 3)) {
         return false;
     }
     tarefas[posicao] = newText;
@@ -50,7 +80,18 @@ function editarTarefa(posicao) {
     salvarDados();
 }
 function deletarTarefa(posicao) {
-    tarefas.splice(posicao, 1);
+    tarefas.splice(posicao, 2);
+    listarTarefas();
+    salvarDados();
+}
+function moverTarefa(posicao) {
+    let stage = Number(tarefas[posicao + 1]);
+    if (stage < 2) {
+        tarefas[posicao + 1] = String(stage + 1);
+    }
+    else {
+        tarefas[posicao + 1] = "0";
+    }
     listarTarefas();
     salvarDados();
 }
--- Tarefas/src/index.ts
+++ Tarefas/src/index.ts
@@ -1,39 +1,80 @@
-let listElement = document.querySelector("#app ul") as HTMLUListElement;
-let inputElement = document.querySelector("#app input") as HTMLInputElement;
-let buttonElement = document.querySelector("#app button") as HTMLElement;
+//Inputs
+let inputElement = document.querySelector("input") as HTMLInputElement;
+let selectElement = document.querySelector("#stage") as HTMLSelectElement;
+let buttonElement = document.querySelector("#add") as HTMLElement;
 
+//Listas To Do, In Progress e Done
+let todoElement = document.querySelector(".toDo ul") as HTMLUListElement;
+let inProgressElement = document.querySelector(".inProgress ul") as HTMLUListElement;
+let doneElement = document.querySelector(".done ul") as HTMLUListElement;
+
+//Levantamento dos dados salvos
 let listaSalva: (string | null) = localStorage.getItem("@listagemTarefas");
 let tarefas: string[] = listaSalva !== null && JSON.parse(listaSalva) || [];
 
 function listarTarefas() {
-	listElement.innerHTML = "";
+	todoElement.innerHTML = "";
+	inProgressElement.innerHTML = "";
+	doneElement.innerHTML = "";
 
 	tarefas.map( (item, index) => {
-		let todoElement = document.createElement("li");
-		let tarefaText = document.createTextNode(item);
+		/**
+		 * Indices pares.. = Texto da tarefa
+		 * Indices impares = Estágio da tarefa: { "0" | "1" | "2" }
+		 */
+		if(index % 2 == 0){
+			//Elemento <li>
+			let liElement = document.createElement("li");
+			let liText = document.createTextNode(item);
 
-		let editElement = document.createElement("a");
-		let editText = document.createTextNode("Editar");
+			//<div> p/ os links
+			let aDiv = document.createElement("div");
 
-		let deleteElement = document.createElement("a");
-		let deleteText = document.createTextNode("Excluir");
+			//Links de edição e exclusão
+			let editElement = document.createElement("a");
+			let editText = document.createTextNode("Editar");
+			let deleteElement = document.createElement("a");
+			let deleteText = document.createTextNode("Excluir");
+			let moveElement = document.createElement("a");
+			let spanElement = document.createElement("span");
 
-		editElement.setAttribute("href", "#");
-		editElement.setAttribute("onclick", `editarTarefa(${index})`);
-		editElement.setAttribute("style", "color: Orange;");
-		
-		deleteElement.setAttribute("href", "#");
-		deleteElement.setAttribute("onclick", `deletarTarefa(${index})`);
-		deleteElement.setAttribute("style", "color: Red;");
+			editElement.setAttribute("href", "#");
+			editElement.setAttribute("onclick", `editarTarefa(${index})`);
+			editElement.setAttribute("style", "color: Orange;");
+			editElement.appendChild(editText);
 
-		editElement.appendChild(editText);
-		deleteElement.appendChild(deleteText);
-		
-		todoElement.appendChild(tarefaText);
-		todoElement.appendChild(editElement);
-		todoElement.appendChild(deleteElement);
-		
-		listElement.appendChild(todoElement);
+			deleteElement.setAttribute("href", "#");
+			deleteElement.setAttribute("onclick", `deletarTarefa(${index})`);
+			deleteElement.setAttribute("style", "color: Red;");
+			deleteElement.appendChild(deleteText);
+			
+			spanElement.innerHTML = "&#10149;";
+
+			moveElement.setAttribute("href", "#");
+			moveElement.setAttribute("onclick", `moverTarefa(${index})`);
+			moveElement.setAttribute("style", "color: Black;");
+			moveElement.setAttribute("class", "arrow");
+			moveElement.appendChild(spanElement);
+
+			aDiv.appendChild(editElement);
+			aDiv.appendChild(deleteElement);
+			aDiv.appendChild(moveElement);
+			
+			liElement.appendChild(liText);
+			liElement.appendChild(aDiv);
+			
+			switch(tarefas[index+1]){
+				case "0":
+					todoElement.appendChild(liElement);
+					break;
+				case "1":
+					inProgressElement.appendChild(liElement);
+					break;
+				case "2":
+					doneElement.appendChild(liElement);
+					break;
+			}
+		}
 	});
 }
 
@@ -42,11 +83,13 @@ function adicionarTarefa() {
 		alert("Digite alguma tarefa!")
 		return false;
 	} else {
-
 		let tarefaDigitada: string = inputElement.value;
-		tarefas.push(tarefaDigitada);
+		let estagioTarefa: string = selectElement.value;
+
+		tarefas.push(tarefaDigitada, estagioTarefa);
 
 		inputElement.value = "";
+		selectElement.selectedIndex = 0;
 
 		listarTarefas();
 		salvarDados();
@@ -56,7 +99,7 @@ function adicionarTarefa() {
 function editarTarefa(posicao: number) {
 	let newText = prompt("Renomeie a tarefa:");
 
-	if(newText!.length < 3){
+	if((typeof(newText) !== "string") || (newText!.length < 3)){
 		return false;
 	}
 
@@ -67,7 +110,20 @@ function editarTarefa(posicao: number) {
 }
 
 function deletarTarefa(posicao: number) {
-	tarefas.splice(posicao, 1);
+	tarefas.splice(posicao, 2);
+
+	listarTarefas();
+	salvarDados();
+}
+
+function moverTarefa(posicao: number) {
+	let stage = Number(tarefas[posicao+1]);
+
+	if(stage < 2){
+		tarefas[posicao+1] = String(stage+1);
+	}else{
+		tarefas[posicao+1] = "0";
+	}
 
 	listarTarefas();
 	salvarDados();
